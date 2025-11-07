8bf332bf3a74bbc56854c2ff10fba9fb9c518c47
Lauren Miller <lmiller08@qub.ac.uk>
2025-11-07 15:49:43 +0000

    Added show.html

--- tests/test_app.py
+++ tests/test_app.py
@@ -30,14 +30,14 @@ def test_get_album(db_connection, page, test_web_address):
     db_connection.seed("seeds/music_library.sql")
     page.goto(f"http://{test_web_address}/albums/1")
 
-    h3_tags = page.locator("h3")
-    expect(h3_tags).to_have_text([
-        "Title: Surfer Rosa"
+    h1_tags = page.locator("h1")
+    expect(h1_tags).to_have_text([
+        "Surfer Rosa"
     ])
 
     paragraph_tags = page.locator("p")
     expect(paragraph_tags).to_have_text([
-        "Released: 1988"
+        "Release year: 1988"
     ])
     
 # When I call GET /artists
